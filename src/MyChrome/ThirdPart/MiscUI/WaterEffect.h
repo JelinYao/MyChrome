58f56edab904587f04c55e1ef2bed4b3ac686cfb
MX-Qulin <39642357+MX-Qulin@users.noreply.github.com>
2020-07-16 11:57:21 +0800

    Update WaterEffect.h
    VS2015报错,加上这行就好了??

--- src/MyChrome/ThirdPart/MiscUI/WaterEffect.h
+++ src/MyChrome/ThirdPart/MiscUI/WaterEffect.h
@@ -17,7 +17,7 @@
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //
 #pragma once
-
+#include <memory>
 #define random( min, max ) (( rand() % (int)((( max ) + 1 ) - ( min ))) + ( min ))
 
 /**
